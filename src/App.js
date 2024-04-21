ad1fb8b146d23339ff4e177f381f2a86c0d54c7c
MarinRancic <marin.rancic1@gmail.com>
2024-04-21 22:13:17 +0200

    few bug fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Footer from "./Components/UI/Footer";
 import Header from "./Components/UI/Header/Header";
+import AutoScrollToTop from "./Components/Utils/AutoScrollToTop";
 import ProductDetailsPage from "./Pages/ProductDetailsPage";
 import ProductsPage from "./Pages/ProductsPage";
 import { Route, Routes } from "react-router-dom";
@@ -8,10 +9,12 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
+      <AutoScrollToTop />
       <Header />
       <Routes>
         <Route index element={<ProductsPage />} />
         <Route path="/product/:id" element={<ProductDetailsPage />} />
+        <Route path="*" element={<ProductsPage />} />
       </Routes>
       <Footer />
     </>
